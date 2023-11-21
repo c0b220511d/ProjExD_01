06c8671496cb740608738e8eea5644cb1eb24d4d
c0b22051 <c0b220511d@edu.teu.ac.jp>
2023-11-21 16:14:33 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1：背景画像Surfaceの生成
+    bg_img2 =  pg.transform.flip(bg_img, True, False) #演習2:背景画像を左右反転
     kk_img = pg.image.load("ex01/fig/3.png") #練習2:こうかとん画像Surfaceの生成
     kk_img = pg.transform.flip(kk_img, True, False) #練習2:こうかとんを左右反転
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] #練習3:こうかとんSurfaceのリスト
@@ -17,7 +18,7 @@ def main():
 
         x = tmr%1600 #練習6:動く背景画像
         screen.blit(bg_img, [-x, 0]) #練習4：背景画像の表示
-        screen.blit(bg_img, [1600-x, 0]) #練習6:動く背景画像
+        screen.blit(bg_img2, [1600-x, 0]) #練習6:動く背景画像
         screen.blit(kk_imgs[tmr%2], [300, 200]) #練習5:こうかとんはばたく
         
         pg.display.update()
